8525bfdce0bd738643f245224738ccb3f9213f4d
Samat Murat <muratsamat090598@gmail.com>
2020-10-01 13:33:54 +0600

    Добавил тесты

--- TestApp2/Controllers/HomeController.cs
+++ TestApp2/Controllers/HomeController.cs
@@ -10,28 +10,16 @@ using TestApp2.Models;
 namespace TestApp2.Controllers
 {
     public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+    { 
         public IActionResult Index()
         {
+            ViewData["Message"] = "Hello world!";
             return View();
         }
 
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
         }
     }
 }
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using TestApp2.Controllers;
+using Xunit;
+
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void IndexViewDataMessage()
+        {
+            HomeController controller = new HomeController();
+            
+            ViewResult result = controller.Index() as ViewResult;
+            
+            Assert.Equal("Hello world!", result?.ViewData["Message"]);
+        }
+    }
+}
